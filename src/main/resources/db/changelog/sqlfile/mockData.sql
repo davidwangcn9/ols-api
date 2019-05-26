ae98ecb376017f2a2ef742986fe787644d12724b
yuling.liu@thoughtworks.com <YulingLiu>
2019-05-26 13:33:27 +0800

    Refactor mock data

--- src/main/resources/db/changelog/sqlfile/mockData.sql
+++ src/main/resources/db/changelog/sqlfile/mockData.sql
@@ -1,8 +1,10 @@
-INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '哈哈', '呵呵','2019-05-03',1558791724499);
-INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '哈哈', '呵呵','2019-05-03',1558791724499);
-INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '哈哈', '呵呵','2019-05-03',1558791724499);
-INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '哈哈', '呵呵','2019-05-03',1558791724499);
-INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '哈哈', '呵呵','2019-05-03',1558791724499);
-INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '哈哈', '呵呵','2019-05-03',1558791724499);
-INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '哈哈', '呵呵','2019-05-03',1558791724499);
-INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '哈哈', '呵呵','2019-05-03',1558791724499);
+INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), 'NHO-新人训练营', '京东敏捷实战特训，京东内部的高绩效团队培训，真正的敏捷培训，为你深度分享用户故事、敏捷估算、敏捷项目计划及跟踪（任务看板，每日会议）等敏捷实践。 ','2019-06-03',1558791724499);
+INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '敏捷训练营', '京东敏捷实战特训，京东内部的高绩效团队培训，真正的敏捷培训，为你深度分享用户故事、敏捷估算、敏捷项目计划及跟踪（任务看板，每日会议）等敏捷实践。 ','2019-07-03',1558792724499);
+INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '测试训练营', '京东敏捷实战特训，京东内部的高绩效团队培训，真正的敏捷培训，为你深度分享用户故事、敏捷估算、敏捷项目计划及跟踪（任务看板，每日会议）等敏捷实践。 ','2019-08-03',1558793724499);
+INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), 'Scrum训练营', '京东敏捷实战特训，京东内部的高绩效团队培训，真正的敏捷培训，为你深度分享用户故事、敏捷估算、敏捷项目计划及跟踪（任务看板，每日会议）等敏捷实践。 ','2019-09-03',1558794724499);
+INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '数据训练营', '京东敏捷实战特训，京东内部的高绩效团队培训，真正的敏捷培训，为你深度分享用户故事、敏捷估算、敏捷项目计划及跟踪（任务看板，每日会议）等敏捷实践。 ','2019-10-03',1558795724499);
+INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '敏捷训练营', '京东敏捷实战特训，京东内部的高绩效团队培训，真正的敏捷培训，为你深度分享用户故事、敏捷估算、敏捷项目计划及跟踪（任务看板，每日会议）等敏捷实践。 ','2019-11-03',1558796724499);
+INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '敏捷训练营', '京东敏捷实战特训，京东内部的高绩效团队培训，真正的敏捷培训，为你深度分享用户故事、敏捷估算、敏捷项目计划及跟踪（任务看板，每日会议）等敏捷实践。 ','2019-12-03',1558797724499);
+INSERT INTO training_camp (id, title, description, start_time, created_time) VALUES (RANDOM_UUID(), '敏捷训练营', '京东敏捷实战特训，京东内部的高绩效团队培训，真正的敏捷培训，为你深度分享用户故事、敏捷估算、敏捷项目计划及跟踪（任务看板，每日会议）等敏捷实践。 ','2020-04-03',1558798724499);
+
+
